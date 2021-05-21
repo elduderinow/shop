3c160d0bc04d43c43f67b847e79f488bb9c119e4
elduderinow <yarrut.franken@gmail.com>
2021-05-21 08:49:11 +0200

    first push, initializing files, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# shop
+# Title: Order Form
+
+Creating a Pokedex in HTML and CSS and javascript.
+
+- Repository: `shop`
+- Link github: https://github.com/elduderinow/shop
+- Language: HTML, SCSS, PHP, JS
+
+## WHAT IS THE SHOP
+//
+
+## PURPOSE
+The purpose of the creation of this shop is to learn to work with PHP super globasl, ie: $GET, $POST, $COOKIE, SESSIONS etc. Be able to write a validation and send an email with PHP.
+
+## FUNCTIONALITIES
+- //
+
+## PROGRESS
+- 
+
+
+## THE PROJECT ITSELF & MY OPINION
+//
+
---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Candy Shop</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Extra links -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<h1>Welcome to the candy shop</h1>
+<p>I'll let you pick a lolly pop</p>
+</body>
+
+<!-- Optional JavaScript -->
+
+<script src="resources/functions.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/v4-shims.min.js"></script>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://ajax.aspnetcdn.com/ajax/bootstrap/4.3.1/bootstrap.min.js"></script>
+</html>
